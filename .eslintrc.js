b03362bc3f660d4d6501cf93b827d5dd495d41e3
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-04-21 18:40:39 +0300

    added eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+[
+  {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+    },
+    parserOptions: {
+      ecmaVersion: 12,
+    },
+    rules: {
+      "no-console": "off",
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+    },
+  },
+  "google",
+];
